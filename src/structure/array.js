aa09ebb8d454a62885d86cb059f8a36e8236d08a
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2021-01-14 00:28:30 +0100

    updates

--- src/structure/array.js
+++ src/structure/array.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { 
     FormGroup,
     Label,
@@ -8,25 +8,53 @@ import {
 const Array = () => {
 
     const [length, setLength] = useState(0)
+    const [array, setArray] = useState([])
 
     const appStyleArray = {
         textAlign: 'center',
         marginTop: '10px',
-        display: 'block'
+        // display: 'block'
       }
     
+    const arraybar = {
+        backgroundColor: 'yellow',
+        display: 'inline',
+        margin: '0 1px',
+        border: '1px solid'
+    }
+    const randomNumber = (min, max) => {
+        return Math.floor(Math.random() * (max - min + 1) + min );
+    }
+    
+    const shape = array.map((value, index) => (
+        <>
+        <div style={arraybar} key={index} style={{height: `${value*2}px`, width: '20px' }}>{value}</div>
+        </>
+    ))
+    
+    useEffect(() => {
+        for (let i=0; i < length/4; i++ ) {
+            array.push(randomNumber(1, length/2))
+        }
+        console.log(length)
+    }, [length, array])
 
     const handleSlider = (event) => {
-        setLength(event.target.value)
-        console.log(event.target.value)
+        if(event.target.value !== length) {
+            // setArray([])
+            setLength(event.target.value)
+        }
     }
     return (
         <>
         <div style={appStyleArray}>
             <FormGroup>
-                <p style={{margin: "10px"}}><Label for="range">Select Array Length: {length}</Label></p>
+                <p style={{margin: "10px"}}><Label for="range">Select Array Length: {Math.ceil(length/4)}</Label></p>
                 <Input style={{width: "40%"}} type="range" name="range" id="range" defaultValue={0} onChange={handleSlider}/>
             </FormGroup>
+        </div>
+        <div>
+            {shape}
         </div>
         
         </>
