0664a987d07ed57843ee0821720f1ee5ce5f7ce5
Ziyad Mourabiti <mourabitiziyad@gmail.com>
2021-01-14 05:20:49 +0100

    changed menu

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,9 +6,9 @@ const Navbar = () => {
         <a style={{fontSize: "25px"}} href="#">AlgoViz</a>
         <nav>
             <ul className="nav__links">
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Projects</a></li>
-                <li><a href="#">About</a></li>
+                <li><a href="#">Arrays</a></li>
+                <li><a href="#">Graphs</a></li>
+                <li><a href="#">Trees</a></li>
             </ul>
         </nav>
         <a className="cta" href="#"><button>Contact</button></a>
